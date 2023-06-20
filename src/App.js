68f4a5cdf899fe48138dec9b49815781ed6854c2
Tri Bayu Priangga <tpriangga6@gmail.com>
2023-06-20 22:39:00 +0700

    Create Header Component and put to arrange landingpage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import LandingPage from 'pages/LandingPage';
+import React from 'react';
+import { BrowserRouter as Switch, Route } from 'react-router-dom';
+import "assets/scss/style.scss";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">
+      <Switch>
+        <Route path="/" component={LandingPage}></Route>
+      </Switch>
+    </div>;
 }
 
 export default App;
